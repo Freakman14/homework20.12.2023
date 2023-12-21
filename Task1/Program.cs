04733e129186896f6bfad170cb52766e50071813
Freakman14 <freemem13@gmail.com>
2023-12-21 14:15:55 +0500

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,92 @@
+﻿
+
+var teach = new Teacher();
+teach.FullName = "Shahrom";
+teach.Subject = "mathematics";
+teach.Experience = 20;
+
+System.Console.WriteLine(teach.Teach());
+System.Console.WriteLine(teach.Teach1());
+
+
+public class Teacher
+{
+    private string fullName;
+    private string _subject;
+    private int _experience;
+
+    public Teacher()
+    {
+
+    }
+
+    public Teacher(string name, string subject)
+    {
+        fullName = name;
+        _subject = subject;
+    }
+
+    public Teacher(string name, string subject, int experience)
+    {
+        fullName = name;
+        _subject = subject;
+        _experience = experience;
+    }
+
+    public string Teach()
+    {
+        return $"{fullName} is teaching {_subject}.";
+    }
+    public string Teach1()
+    {
+        return $"She study {_experience} years.";
+    }   
+
+    public void SetExperience(int years)
+    {
+        _experience = years;
+    }
+
+    public int GetExperience()
+    {
+        return _experience;
+    }
+
+    public string GetFullName()
+    {
+        return fullName;
+    }
+
+    public string GetSubject()
+    {
+        return _subject;
+    }
+
+    public void SetFullName(string name)
+    {
+        fullName = name;
+    }
+
+    public void SetSubject(string subject)
+    {
+        _subject = subject;
+    }
+
+    public int Experience
+    {
+        get { return _experience; }
+        set { _experience = value; }
+    }
+
+    public string FullName
+    {
+        get { return fullName; }
+        set { fullName = value; }
+    }
+
+    public string Subject
+    {
+        get { return _subject; }
+        set { _subject = value; }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,79 @@
+﻿
+
+var s1 = new Student();
+s1.FullName = "Abdullo";
+s1.Age = 24;
+s1.Grade = 12;
+
+s1.Study();
+System.Console.WriteLine(s1.GetAge());
+System.Console.WriteLine(s1.GetGrade());
+public class Student
+{
+    private string fullname;
+    private int _age;
+    private int _grade;
+
+    public Student(string name, int age)
+    {
+        fullname = name;
+        _age = age;
+    }
+    public Student(string name, string subject, int grade)
+    {
+        fullname = name;
+        _grade = grade;
+    }
+
+    public Student()
+    {
+    }
+
+    public void Study()
+    {
+        System.Console.WriteLine($"{fullname} is studying.");
+    }
+    public void SetGrade(int newGrade)
+    {
+        _grade = newGrade;
+    }
+    public int GetGrade()
+    {
+        return _grade;
+    }
+
+
+    public string Getfullname()
+    {
+        return fullname;
+    }
+    public int GetAge()
+    {
+        return _age;
+    }
+    public void Setfullname(string name)
+    {
+        fullname = name;
+    }
+    public void SetAge(int age)
+    {
+        _age = age;
+    }
+
+    public string FullName
+    {
+        get { return fullname; }
+        set { fullname = value; }
+    }
+    public int Age
+    {
+        get { return _age; }
+        set { _age = value; }
+    }
+    public int Grade
+    {
+        get { return _grade; }
+        set { _grade = value; }
+    }
+
+}
